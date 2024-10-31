e855dbaee52282edcc37f805c90c6464eab5d035
yoshida <mein777dedede@gmail.com>
2024-11-01 00:58:33 +0900

    トリマ完成（あとはデバッグ）

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Text.Json.Serialization;
 
 namespace Network
@@ -10,10 +10,10 @@ namespace Network
     class Network_sys{
         
         public const string KEY = "aaa";
-        private static readonly HttpClient client = new HttpClient();
+        private readonly HttpClient client ;
         private string table_id, table_hash;
 
-        private const string domain = "slottools.japaneast.cloudapp.azure.com";
+        public const string domain = "slottools.japaneast.cloudapp.azure.com";
         public string get_Table_id(){
             return table_id;
         }
@@ -21,6 +21,9 @@ namespace Network
             return table_hash;
         }
         public Network_sys(string table_id, string table_hash){
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+            client = new HttpClient(handler);
             this.table_hash = table_hash;
             this.table_id   = table_id;
         }
@@ -108,7 +111,7 @@ namespace Network
                 TableHash = this.TableHash,
                 Probability = update
             };
-            var resp = await ns.post_method<TableResp>(table,"/table_probability");
+            var resp = await ns.post_method<TableResp>(table,"/update_probability");
             if("Success".Equals(resp.Result)){
                 return true;
             }
@@ -210,7 +213,7 @@ namespace Network
             };
             this.ns = ns;
         }
-        //現在の「金」を入れてほしい（球数じゃなくて　計算式知らんし） 
+        //現在の「金」を入れてほしい（球数ではない　計算式知らんし） 
         public async Task<bool> update_money(int money){
             var tmp = user.Money;
             user.Money = money;
@@ -231,18 +234,144 @@ namespace Network
                 throw new Exception("アップデートエラー、管理者に問い合わせてください");
             }
         }
+        //ログアウトのリクエストを送る
+        //最初の一回だけ送って
+        public async Task<bool> logout_request(){
+             var resp = await ns.post_method<UserResult>(user, "/token_exists");
+            if("success".Equals(resp.Result)){
+                return true;
+            }else{
+                throw new Exception("アップデートエラー、管理者に問い合わせてください");
+            }
+        }
+
+
+        //ログアウトリクエストが承認されたかを確かめるメソッド
+        //間違えても↑送んないでね★
+        public async Task<bool> logout_isdone(){
+             var resp = await ns.post_method<UserResult>(user, "/user_Logout");
+            if("success".Equals(resp.Result)){
+                return "Logout successful".Equals(resp.Message);
+            }else{
+                throw new Exception("アップデートエラー、管理者に問い合わせてください");
+            }
+        }
+        public class Result_resp
+        {
+            [JsonPropertyName("success")]
+            public bool Success { get; set; }
+
+            [JsonPropertyName("result")]
+            public string? Result { get; set; }
+        }
+
+        //入金リクエスト
+        //↑と同じく、最初の一回だけ送って
+        public async Task<bool> request_payment(){
+            var req = new {
+                username = user.Username,
+                table_id = user.Table
+            };
+            var resp = await ns.post_method<Result_resp>(req, "/request-payment-handle");
+            return resp.Success;
+        }
+
+        public async Task<bool> request_payment_exists(){
+            var req = new {
+                username = user.Username,
+                table_id = user.Table
+            };
+            var resp = await ns.post_method<Result_resp>(req, "/request-payment-exists");
+            return resp.Success;
+        }
+
+        public async Task<bool> Bonus_result(int Money){
+            var bonus = new {
+                money = Money,
+                location = ns.get_Table_id()  
+            };
+            var resp = await ns.post_method<Result_resp>(bonus, "/Bonus_result_append");
+            return resp.Success;
+        }
     }
 
 
     //作れたら作る
     class Log_Sender{
+        public class SendLog
+        {
+            [JsonPropertyName("level")]
+            public int Level { get; set; }
+        
+            [JsonPropertyName("location")]
+            public string Location { get; set; }
+        
+            [JsonPropertyName("message")]
+            public string Message { get; set; }
+        }
+        private Network_sys net;
+        private Queue<SendLog> queue;
+        private Timer timer;
+        
+        private  readonly HttpClient client;
 
+        private async void Execute(object state){
+            Console.Write("よびだされ");
+            if (queue.Count != 0){
+                StringBuilder result = new StringBuilder("[");
+                SendLog s;
+                while (queue.Count != 0)
+                {
+                    s = queue.Dequeue();
+                    result.Append(JsonSerializer.Serialize(s)).Append(',');
+                }
+                result.Remove(result.Length - 1, 1).Append("]");
+                Console.WriteLine(result.ToString());
+                var jsonContent = new StringContent(result.ToString(), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("https://"+ Network_sys.domain + "/api/add_log_file", jsonContent);
+                response.EnsureSuccessStatusCode();
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                if("ALL DONE".Equals(jsonResponse)){
+                    Console.Write("書き込み完了");
+                }else Console.Write("鰓");
+            }
+        } 
+        public Log_Sender(Network_sys ns){
+            
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+
+            client = new HttpClient(handler);
+            this.net = ns;
+            this.queue = new Queue<SendLog>();
+            timer = new System.Threading.Timer(Execute, null, 0, 1000);
+        }
+        public void log_print(int level, string message){
+            var log = new SendLog(){
+                Level = level,
+                Location = net.get_Table_id(),
+                Message = message
+            };
+            queue.Enqueue(log);
+        }
+        public void Dispose(){
+            timer.Dispose();
+        }
+        
     }
 
     // See https://aka.ms/new-console-template for more information
     internal class Myprogram{
-        static void Main(String[] args){ 
-            Console.WriteLine("Hello, World!");
+        static void Main(String[] args){
+            
+            var ns = new Network_sys("table_120","21382800000aasssbb");
+            var ls = new Log_Sender(ns);
+            for(int i = 0; i < 3; i++){
+                ls.log_print(i, "Hello World");
+            }
+            Console.WriteLine("エンターで終了");
+            Console.ReadLine();
         }
     }
 }
